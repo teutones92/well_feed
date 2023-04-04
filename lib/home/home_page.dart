485e297862cba58faee58ef4f292c1e71ae67b72
teutones92 <teutones92@gmail.com>
2023-04-04 00:30:00 -0400

    working on dising of home page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:well_feed/blocs/home_bloc.dart';
+import 'package:well_feed/models/home_view_data/home_view_data.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key, required this.userData, required this.homeBloc});
@@ -8,6 +9,7 @@ class HomePage extends StatelessWidget {
   final HomeBloc homeBloc;
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         leading: Padding(
@@ -34,9 +36,66 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: Stack(
+        fit: StackFit.expand,
         children: [
           Container(
-              decoration: BoxDecoration(color: Colors.green), child: Column()),
+            decoration: BoxDecoration(color: Colors.green.shade800),
+            child: Image.asset(
+              'assets/bkg_well_feed.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          ListView(
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    '5',
+                    style: TextStyle(fontSize: size.height / 4.5),
+                  ),
+                  SizedBox(
+                    width: size.height / 4,
+                    child: Text(
+                      style: TextStyle(fontSize: size.height / 30),
+                      'strategies to success'.toUpperCase(),
+                      // overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children:
+                    List.generate(HomeViewData.homeviewData.length, (index) {
+                  final item = HomeViewData.homeviewData[index];
+                  return Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 4, horizontal: 25),
+                    child: Card(
+                      elevation: 3,
+                      color: item.color,
+                      child: Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: ListTile(
+                          leading: Image.asset(
+                            item.imagePath,
+                            fit: BoxFit.cover,
+                          ),
+                          title: Text(item.title),
+                          subtitle: Text(
+                            item.content,
+                            // softWrap: true,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 4,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ],
+          )
         ],
       ),
     );
--- lib/models/home_view_data/home_view_data.dart
+++ lib/models/home_view_data/home_view_data.dart
@@ -1,36 +1,53 @@
+import 'package:flutter/material.dart';
+
 class HomeViewData {
   late String title;
   late String content;
-  late String imagePad;
+  late Color color;
+  late String imagePath;
 
-  HomeViewData(
-      {required this.title, required this.content, required this.imagePad});
+  HomeViewData({
+    required this.title,
+    required this.content,
+    required this.color,
+    required this.imagePath,
+  });
 
   static List<HomeViewData> homeviewData = [
     HomeViewData(
-      title: '',
-      content: '',
-      imagePad: '',
+      title: 'Yoga',
+      content:
+          'Yoga is a sophisticated, age-old discipline that has its roots in Indian philosophy. Although it started out as a spiritual practice, it has gained popularity as a means of fostering both physical and emotional wellbeing.',
+      imagePath: 'assets/images_home/yoga_w_sited.png',
+      color: Colors.green.shade600,
     ),
     HomeViewData(
-      title: '',
-      content: '',
-      imagePad: '',
+      title: 'Vegetables',
+      content:
+          'Any veggie, or juice made entirely of vegetables, falls under the category of vegetables. Fresh, refrigerated, canned, dried, or cooked vegetables are all acceptable. They may be served whole, chopped, or pureed. Dark green, red and orange, beans, peas, and lentils; starchy; and other veggies are the five subgroups of vegetables that are categorized according to their nutritional value.',
+      imagePath: 'assets/images_home/vegetable_w_hart.png',
+      color: Colors.lightGreen.shade300,
     ),
     HomeViewData(
-      title: '',
-      content: '',
-      imagePad: '',
+      title: 'Sleep',
+      content:
+          'Sleep is a regular, reversible condition of decreased responsiveness to external stimulation accompanied by intricate and predictable physiologic changes. Among these modifications are variations in hormone levels, relaxation of the musculature, and coordinated, spontaneous, internally produced brain activity.',
+      imagePath: 'assets/images_home/sleep_w.png',
+      color: Colors.green.shade600,
     ),
     HomeViewData(
-      title: '',
-      content: '',
-      imagePad: '',
+      title: 'Meat & Fruit',
+      content:
+          'Since renowned podcaster Joe Rogan decided to stick to a strict meat-and-fruit diet for an entire month in January of last year, the meat-and-fruit diet has gained popularity. But nothing about this is brand-new or unexpected. In reality, every January, Rogan has been observed adopting a carnivore diet. ',
+      imagePath: 'assets/images_home/food_w.png',
+      color: Colors.lightGreen.shade300,
     ),
     HomeViewData(
-      title: '',
-      content: '',
-      imagePad: '',
+      title: 'Weigth',
+      content:
+          'Depending on your level of exercise and other variables, your body requires a specific number of calories per day to maintain your current weight. Tell us which one would be ideal for you.',
+      imagePath: 'assets/images_home/weigth_w.png',
+      color: Colors.green.shade600,
     ),
   ];
 }
